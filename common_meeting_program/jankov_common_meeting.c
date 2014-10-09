3646cd9226db4c297c3525f30020a996916fb915
Viktor Jankov <viktormiloshjankov@gmail.com>
2014-10-09 12:40:46 -0500

    made program compatible with sand compiler

--- common_meeting_program/jankov_common_meeting.c
+++ common_meeting_program/jankov_common_meeting.c
@@ -10,10 +10,11 @@
 void *compare_times(void * arg) {
 	int compare_value = * (int *) arg;
 	free(arg);
-
-	for(int i = 0; i < second_array_size; i++) {
+	int i;
+	int j;
+	for(i = 0; i < second_array_size; i++) {
 		if( second[i] == compare_value) {
-			for(int j = 0; j < third_array_size; j++) {
+			for(j = 0; j < third_array_size; j++) {
 				if( compare_value == third[j]) {
 					printf("%d is a common meeting time.\n", compare_value);
 				}
@@ -28,7 +29,8 @@ void fill_array(FILE *myFile, int **array, int *array_size) {
 	fscanf(myFile, "%d", &value);
 	*array_size = value;
 	*array = (int *) malloc(sizeof(int) * value);
-	for(int i = 0; i < *array_size; i++) {
+	int i;
+	for(i = 0; i < *array_size; i++) {
 		fscanf(myFile, "%d", &(*array)[i]);
 	}
 }
@@ -44,12 +46,13 @@ int main(int argc, char *argv[])
 	fill_array(myFile, &second, &second_array_size);
 	fill_array(myFile, &third, &third_array_size);
 
+	int i;
 	pthread_t *tid[first_array_size];
-	for(int i = 0; i < first_array_size; i++) {
+	for(i = 0; i < first_array_size; i++) {
 		tid[i] = (pthread_t *) malloc(sizeof(pthread_t));
 	}
 
-	for(int i = 0; i < first_array_size; i++) {
+	for(i = 0; i < first_array_size; i++) {
 		int *the_parm = (int *) malloc(sizeof(int));
 		*the_parm = first[i];
 		if( pthread_create( tid[i], NULL,
@@ -61,7 +64,7 @@ int main(int argc, char *argv[])
 	}
 
 	void **thread_ret_val = (void *) malloc (sizeof (void*));
-	for(int i = 0; i < first_array_size; i++) {
+	for(i = 0; i < first_array_size; i++) {
 		if(pthread_join(*tid[i], thread_ret_val)) {
 			fprintf(stderr, "Error joining with thread %d", i);
 		}
